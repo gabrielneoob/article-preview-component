9dfa750e828d1d25af8f477739848783daa48aa4
Gabriel Neoob <gabrielngc@gmail.com>
2022-02-06 23:25:10 -0300

    fit: share icon script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,14 @@
 let icon = document.querySelector('.icon-share-img');
 let shareWrapper = document.querySelector('.container-js');
+let shareIcon = document.querySelector('.icon-share-img');
 icon.addEventListener('click', (() => {
   shareWrapper.classList.toggle('active');
-  icon.classList.toggle('a');
+  if(shareIcon.childNodes[1].classList.contains('a')) {
+    shareIcon.childNodes[1].classList.remove('a');
+  } else {
+    shareIcon.childNodes[1].classList.add('a')
+  }
+  icon.classList.toggle('bg-dark')
 }));
 
 const h1 = document.getElementsByTagName('h1')[0];
